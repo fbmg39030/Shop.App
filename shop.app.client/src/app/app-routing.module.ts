b90b87a5768714fc70574bcf642cce4f86157e9f
Fabian Moling <fabian.mf11@gmail.com>
2023-12-29 20:37:55 +1100

    Add home and cart component with router

--- shop.app.client/src/app/app-routing.module.ts
+++ shop.app.client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AvailableProductsComponent } from './component/available-products/available-products.component';
+import { CartComponent } from './component/cart/cart.component';
 
 const routes: Routes = [ 
-  { path: "", component: AvailableProductsComponent }
+  { path: "", component: AvailableProductsComponent },
+  { path: "cart", component: CartComponent }
 ];
 
 @NgModule({
--- shop.app.client/src/app/app.module.ts
+++ shop.app.client/src/app/app.module.ts
@@ -6,14 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SHOP_API_BASE_URL } from './clients/shop-client';
 import { AvailableProductsComponent } from './component/available-products/available-products.component';
+import { HeaderComponent } from './component/header/header.component';
+
+//primeng imports
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { CartComponent } from './component/cart/cart.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AvailableProductsComponent
+    AvailableProductsComponent,
+    HeaderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
+    ButtonModule,
+    MenubarModule,
     AppRoutingModule
   ],
   providers: [ {provide: SHOP_API_BASE_URL, useValue: "https://localhost:7058" }],
