28e3e5761fd9a4dcb924c6248f8c9543e45b2cb9
Fabian Moling <fabian.mf11@gmail.com>
2023-12-30 15:23:49 +1100

    Add basic frontend for add products

--- shop.app.client/src/app/app.module.ts
+++ shop.app.client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductClient, SHOP_API_BASE_URL } from './clients/shop-client';
 import { AvailableProductsComponent } from './component/available-products/available-products.component';
 import { HeaderComponent } from './component/header/header.component';
+import { DataManagementComponent } from './component/data-management/data-management.component';
 
 //primeng imports
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { CartComponent } from './component/cart/cart.component';
-import { DataManagementComponent } from './component/data-management/data-management.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FileUploadModule } from 'primeng/fileupload';
 
 
 @NgModule({
@@ -27,7 +33,13 @@ import { DataManagementComponent } from './component/data-management/data-manage
     BrowserModule, HttpClientModule,
     ButtonModule,
     MenubarModule,
+    InputTextModule,
+    FormsModule,
     AppRoutingModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextareaModule,
+    FileUploadModule
   ],
   providers: [ 
     {provide: SHOP_API_BASE_URL, useValue: "http://localhost:8080" },
