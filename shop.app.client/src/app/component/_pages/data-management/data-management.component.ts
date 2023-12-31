5e22065998da01912d1c4637c2e5cc8493d59c48
Fabian Moling <fabian.mf11@gmail.com>
2023-12-31 12:29:40 +1100

    Add products from data management page

--- shop.app.client/src/app/component/_pages/data-management/data-management.component.ts
+++ shop.app.client/src/app/component/_pages/data-management/data-management.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductAddOrUpdateRequest, ProductClient } from '../../../clients/shop-client';
+import { lastValueFrom } from 'rxjs';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-data-management',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './data-management.component.css'
 })
 export class DataManagementComponent {
-  value:string=""
+  productName: string = "";
+  productPrice: string = "";
+  productDescription: string = "";
+
+  constructor(private productClient: ProductClient, private messageService: MessageService) {  }
+
+  public async submit(){
+    let request = new ProductAddOrUpdateRequest();
+    request.name1 = this.productName;
+    request.description = this.productName;
+    request.price = +this.productPrice;
+    
+    try {
+      const result = await lastValueFrom(this.productClient.addOrUpdate(request));
+      if(result!=null){
+        this.messageService.add({"severity":"success", "summary":"Added successfully!", "detail":"Operation was successful!"})
+      }
+    } catch (error) {
+      this.messageService.add({"severity":"error", "summary":"An error occurred", "detail":"Adding product was not possible!"})
+    }
+  }
 }
