52b1d8fbda4fa4388ceaca6b03785b8b92182c29
Fabian Moling <fabian.mf11@gmail.com>
2023-12-31 12:48:15 +1100

    Add "addToCart" functionality

--- shop.app.client/src/app/component/_pages/available-products/available-products.component.ts
+++ shop.app.client/src/app/component/_pages/available-products/available-products.component.ts
@@ -13,33 +13,14 @@ import { DataViewLayoutOptions } from 'primeng/dataview';
 export class AvailableProductsComponent implements OnInit{
   layout:string = ''
   products: ProductDto[] = [];
+  cartLoidList: string[] = [];
+  cartLocalstorageKey: string = "CART_ITEMS";
 
   constructor(private productClient: ProductClient, private messageService: MessageService) { }
 
   async ngOnInit(): Promise<void> {
-    const qp :ProductQp=new ProductQp();    
-    qp.name1="";
-
-    try {    
-      const productsFound = await lastValueFrom(this.productClient.query(qp));
-      this.products = productsFound;
-      
-      this.messageService.add(
-        { 
-          severity: 'success',
-          summary: 'Query successful!',
-          detail: 'Products were queried successfully.'
-        }
-      );
-    } catch (error) {
-      this.messageService.add(
-        { 
-          severity: 'error',
-          summary: 'Query unsuccessful!',
-          detail: 'Something went wrong during the query'
-        }
-      );
-    }
+    this.readLoidsFromLocalstorage();
+    await this.queryProducts();
   }
   getSeverity (product: ProductDto) {
     switch (product.name1) {
@@ -55,6 +36,43 @@ export class AvailableProductsComponent implements OnInit{
         default:
             return 'danger';
     }
-};
+  };
+
+  readLoidsFromLocalstorage(){
+    const result = localStorage.getItem(this.cartLocalstorageKey);
+
+    if(result){
+      this.cartLoidList = JSON.parse(result)
+    }
+  }
+
+  async queryProducts(){
+    const qp :ProductQp=new ProductQp();    
+    qp.name1="";
+
+    try {    
+      const productsFound = await lastValueFrom(this.productClient.query(qp));
+      this.products = productsFound;
+    } catch (error) {
+      this.messageService.add(
+        { 
+          severity: 'error',
+          summary: 'Query unsuccessful!',
+          detail: 'Something went wrong during the query'
+        }
+      );
+    }
+  }
+
+  addToCard(product: ProductDto){
+    if(this.cartLoidList.some(loid => loid == product.logicalObjectId)){
+      this.messageService.add({ "severity": "error", "summary": "Duplicate product", "detail":"Product already in cart!"})
+      return;
+    }
+  
+    this.cartLoidList.push(product!.logicalObjectId!.toString());
+    localStorage.setItem(this.cartLocalstorageKey, JSON.stringify(this.cartLoidList));
+    this.messageService.add({ "severity": "success", "summary": "Operation successful", "detail":"Product added successfully!!"})
+  }
 
 }
